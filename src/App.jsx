b680ee17541abf3538d603f98974a127d37072dd
Daniel Costa <daniel.dc697@gmail.com>
2025-05-07 17:19:26 -0300

    Estruturando a app com o esqueleto - as rotas que serão ainda criadas e estilizadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import SideBar from "./components/SideBar";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <>
-    <SideBar/>
-  </>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/sobrenos" element={<SobreNos />} />
+
+        {/* Rotas protegidas */}
+        <Route element={<RotaPrivada />}>
+          <Route path="/perfil" element={<Perfil />} />
+        </Route>
+
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
