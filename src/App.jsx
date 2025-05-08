7d10e520637ea42a852f54ccfba6645f3473e8ac
Daniel Costa <daniel.dc697@gmail.com>
2025-05-08 17:40:25 -0300

    Retirei a RotaPrivada. Por enquanto que finalizo o contexto de useAuth

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,20 @@ import Perfil from "../src/pages/Perfil";
 
 import Login from "../src/pages/Login";
 import Publicar from "./pages/Publicar";
-import RotaPrivada from "./routes/RotaPrivada";
+
 import Cadastro from "./pages/Cadastro";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Rotas públicas */}
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
 
-        {/* Rotas privadas */}
-        <Route element={<RotaPrivada />}>
-          <Route path="/feed" element={<Feed />} />
-          <Route path="/publicar" element={<Publicar />} />
-          <Route path="/perfil" element={<Perfil />} />
-          <Route path="/sobrenos" element={<SobreNos />} />
-        </Route>
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/publicar" element={<Publicar />} />
+        <Route path="/perfil" element={<Perfil />} />
+        <Route path="/sobrenos" element={<SobreNos />} />
       </Routes>
     </BrowserRouter>
   );
