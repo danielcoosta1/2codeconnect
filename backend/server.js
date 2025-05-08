9d9b52b932fe7e49689268f2c674186aab4360ad
Daniel Costa <daniel.dc697@gmail.com>
2025-05-08 17:37:05 -0300

    Criando as rotas de login/cadastro e  chamando no servidor. Criei também um service para condensar toda a ação no localStorage em um só arquivo no projeto

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
+
+import loginRoutes from "./routes/login.js";
+import cadastroRoutes from "./routes/cadastro.js";
 /* global process */
 const prisma = new PrismaClient();
 
@@ -10,6 +13,8 @@ const PORT = process.env.PORT || 3001;
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use("/usuarios/cadastro", cadastroRoutes); // Usando a rota de cadastro
+app.use("/usuarios/login", loginRoutes); // Usando a rota de login
 
 // Verifica a conexão com o banco de dados
 prisma
