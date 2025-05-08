611f8ae087f66fac3beaa396c5a8c006b3ea75a6
Daniel Costa <daniel.dc697@gmail.com>
2025-05-08 11:53:24 -0300

    Iniciando o esqueleto da estilização da página Publicar

--- src/pages/Publicar/index.jsx
+++ src/pages/Publicar/index.jsx
@@ -1,11 +1,48 @@
-import SideBar from "../../components/SideBar";
+import ContainerMain from "../../components/ContainerMain";
+import {
+  ContainerDireito,
+  ContainerEsquerdo,
+  ContainerImgUpload,
+  ContainerInfoImgUpload,
+  ContainerInfoProjeto,
+  ContainerMainPublicar,
+  ContainerTags,
+  ImgUpload,
+  Tags,
+  UploadButton,
+} from "./styles";
 
 const Publicar = () => {
-    return (
-        <>
-        <SideBar/>
-        </>
-    )
-}
+  return (
+    <ContainerMain>
+      <ContainerMainPublicar>
+        <ContainerEsquerdo>
+          <ContainerImgUpload>
+            <ImgUpload />
+          </ContainerImgUpload>
+          <UploadButton>Carregar Imagem</UploadButton>
+          <ContainerInfoImgUpload>
+            <p>image_projeto.png</p>
+            <img />
+          </ContainerInfoImgUpload>
+        </ContainerEsquerdo>
+        <ContainerDireito>
+          <h2>Novo projeto</h2>
+          <ContainerInfoProjeto>
+            <label>Nome do projeto</label>
+            <input type="text" />
+            <label>Descrição</label>
+            <textarea />
+          </ContainerInfoProjeto>
+          <ContainerTags>
+            <h2>Tags</h2>
+            <input type="text" />
+            <Tags></Tags>
+          </ContainerTags>
+        </ContainerDireito>
+      </ContainerMainPublicar>
+    </ContainerMain>
+  );
+};
 
 export default Publicar;
---
+++ src/pages/Publicar/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const ContainerMainPublicar = styled.section`
+background-color: #171D1F;
+display: flex;
+padding: 2rem;
+
+`;
+
+export const ContainerEsquerdo = styled.div``;
+
+export const ContainerImgUpload = styled.div``;
+
+export const ImgUpload = styled.img``;
+
+export const UploadButton = styled.button``;
+
+export const ContainerInfoImgUpload = styled.div``;
+
+export const ContainerDireito = styled.section``;
+
+export const ContainerInfoProjeto = styled.div``;
+
+export const ContainerTags = styled.div``;
+
+export const Tags = styled.div``;
+
+
+
+
