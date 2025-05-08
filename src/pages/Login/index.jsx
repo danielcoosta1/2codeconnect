1a24758b4c82c9f7c9be645483008a7c8a748013
Daniel Costa <daniel.dc697@gmail.com>
2025-05-08 18:19:40 -0300

    Iniciando a formatação/estilização da página Login, que será a página inicial/"default" da aplicação

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,41 @@
+import {
+  CampoInput,
+  ContainerContent,
+  ContainerDireita,
+  ContainerEsquerda,
+  ContainerInputs,
+  ContainerLoginDigitado,
+  ContainerWrapper,
+} from "./styles";
+
+import imgLogin from "./assets/img_login.png";
+
 const Login = () => {
-    return (
-        <>
-        <h1>teste</h1>
-        </>
-    )
-}
+  return (
+    <ContainerWrapper>
+      <ContainerContent>
+        <ContainerEsquerda>
+          <img src={imgLogin} />
+        </ContainerEsquerda>
+        <ContainerDireita>
+          <ContainerLoginDigitado>
+            <h1>Login</h1>
+            <p>Boas-vindas! Faça seu login</p>
+            <ContainerInputs>
+              <CampoInput>
+                <label htmlFor="email">E-mail </label>
+                <input id="email" type="email" />
+              </CampoInput>
+              <CampoInput>
+                <label htmlFor="senha">Senha </label>
+                <input id="senha" type="password" />
+              </CampoInput>
+            </ContainerInputs>
+          </ContainerLoginDigitado>
+        </ContainerDireita>
+      </ContainerContent>
+    </ContainerWrapper>
+  );
+};
 
 export default Login;
---
+++ src/pages/Login/styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const ContainerWrapper = styled.main`
+  width: 100vw;
+  height: 100vh;
+  background-color: #00090e;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ContainerContent = styled.section`
+  display: flex;
+  gap: 8rem;
+  padding: 2em;
+  background-color: #171d1f;
+  
+`;
+
+export const ContainerEsquerda = styled.div``;
+
+export const ContainerDireita = styled.div`
+  color: #ffffff;
+`;
+
+export const ContainerLoginDigitado = styled.div``;
+
+export const ContainerInputs = styled.div``;
+
+export const CampoInput = styled.div``;
+
+export const ContainerLoginContas = styled.div``;
+
+export const ContainerCadastro = styled.div``;
