b60ad36a4b70344df4ae907bc8a4a6da58e33f4f
Daniel Costa <daniel.dc697@gmail.com>
2025-05-09 14:31:12 -0300

    Estruturando e estilizando a base da página cadastro - formulario para manipulacao dos dados

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,9 +1,92 @@
+import {
+  ButtonSubmitCadastro,
+  CampoInput,
+  ContainerContent,
+  ContainerDireita,
+  ContainerEsquerda,
+  ContainerInputs,
+  ContainerLembrar,
+  ContainerWrapper,
+  FormularioCadastro,
+} from "./styles";
+import { useState } from "react";
+import imgCadastro from "./assets/img_cadastro.png";
+import Checkbox from "../../components/CheckBox";
+
+import iconeArrowForward from "../assets/forward_arrow.svg";
+
 const Cadastro = () => {
-    return (
-        <>
-        <h1>teste</h1>
-        </>
-    )
-}
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const [erro, setErro] = useState("");
+
+  const [lembrarMe, setLembrarMe] = useState(false);
+
+  const [carregando, setCarregando] = useState(false);
+
+  const toggleLembrarMe = () => {
+    setLembrarMe((prev) => !prev); // alterna entre true e false
+  };
+  return (
+    <ContainerWrapper>
+      <ContainerContent>
+        <ContainerEsquerda>
+          <img src={imgCadastro} />
+        </ContainerEsquerda>
+        <ContainerDireita>
+          <h1>Cadastro</h1>
+          <p>Olá! Preencha seus dados abaixo.</p>
+          <FormularioCadastro>
+            <ContainerInputs>
+              <CampoInput>
+                <label htmlFor="nome">Nome</label>
+                <input
+                  type="text"
+                  value={nome}
+                  id="nome"
+                  onChange={(e) => setNome(e.target.value)}
+                  required
+                />
+              </CampoInput>
+              <CampoInput>
+                <label htmlFor="email">E-mail </label>
+                <input
+                  type="email"
+                  value={email}
+                  id="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </CampoInput>
+              <CampoInput>
+                <label htmlFor="senha">Senha </label>
+                <input
+                  type="password"
+                  value={senha}
+                  id="senha"
+                  onChange={(e) => setSenha(e.target.value)}
+                  required
+                />
+              </CampoInput>
+            </ContainerInputs>
+            <ContainerLembrar>
+              <Checkbox
+                checked={lembrarMe}
+                onChange={toggleLembrarMe}
+                label="Lembrar-me"
+              />
+            </ContainerLembrar>
+            <ButtonSubmitCadastro type="submit" disabled={carregando}>
+              {carregando ? <>🔄 Cadastrando...</> : "Entrar"}
+              <img src={iconeArrowForward} />
+            </ButtonSubmitCadastro>
+          </FormularioCadastro>
+        </ContainerDireita>
+      </ContainerContent>
+    </ContainerWrapper>
+  );
+};
 
 export default Cadastro;
---
+++ src/pages/Cadastro/styles.js
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+
+export const ContainerWrapper = styled.main`
+  width: 100vw;
+  height: 100vh;
+  background-color: #00090e;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ContainerContent = styled.section`
+  display: flex;
+  gap: 5rem;
+  padding: 4em;
+  background-color: #171d1f;
+`;
+
+export const ContainerEsquerda = styled.div``;
+
+export const ContainerDireita = styled.div`
+  color: #ffffff;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
+`;
+
+export const FormularioCadastro = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ContainerInputs = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const CampoInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+  font-size: 1rem;
+  input {
+    padding: 0.75rem 1rem;
+    background-color: #888888;
+    border-radius: 8px;
+  }
+`;
+
+export const ContainerLembrar = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 1rem;
+`;
+
+export const ButtonSubmitCadastro = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5em;
+  font-size: 1.25rem;
+  background-color: #81fe88;
+  color: #132e35;
+  border: none;
+  padding: 0.5em 1em;
+  font-weight: 600;
+  border-radius: 8px;
+
+  margin-top: 2rem;
+`;
