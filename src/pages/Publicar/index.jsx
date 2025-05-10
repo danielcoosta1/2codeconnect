70f0645694bfc13460602e5afe6045b303f9f89c
Daniel Costa <daniel.dc697@gmail.com>
2025-05-10 08:41:32 -0300

    Preparando o componente para ler e verificar um arquivo tipo file(img)

--- src/pages/Publicar/index.jsx
+++ src/pages/Publicar/index.jsx
@@ -26,8 +26,41 @@ import iconeTrash from "./assets/icones/trash.svg";
 import arrowForward from "./assets/icones/arrow_forward.svg";
 import arrowUpload from "./assets/icones/arrow_upload.svg";
 import iconeClose from "./assets/icones/close.svg";
+import { useRef, useState } from "react";
 
 const Publicar = () => {
+  const [imagemSelecionada, setImagemSelecionada] = useState(null);
+  const [nomeArquivo, setNomeArquivo] = useState("");
+  const inputRef = useRef();
+
+  const lidarComUpload = (e) => {
+    const arquivo = event.target.files[0];
+
+    if (!arquivo) return;
+
+    const tiposPermitidos = ["image/jpeg", "image/png", "image/jpg"];
+
+    if (!tiposPermitidos.includes(arquivo.type)) {
+      alert("Formato inválido. Use JPEG ou PNG.");
+      return;
+    }
+
+    // Verifica o tamanho do arquivo (limite de 5MB)
+    const tamanhoMaximo = 5 * 1024 * 1024; // 5MB
+    if (arquivo.size > tamanhoMaximo) {
+      alert("Arquivo muito grande. Máximo 5MB.");
+      return;
+    }
+
+    setImagemSelecionada(URL.createObjectURL(arquivo));
+    setNomeArquivo(arquivo.name);
+  };
+
+  const removerImagem = () => {
+    setImagemSelecionada(null);
+    setNomeArquivo("");
+    inputRef.current.value = ""; // limpa o input file
+  };
   return (
     <ContainerMain>
       <ContainerMainPublicar>
