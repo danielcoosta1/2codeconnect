f898c684df11fcc0cc8cd764009f2ada5455d695
Daniel Costa <daniel.dc697@gmail.com>
2025-05-12 10:52:37 -0300

    Aumentando o rendimento colocando uma condição do useEffect rodar - ter usuario logado

--- src/context/publicacao/PublicacaoProvider.jsx
+++ src/context/publicacao/PublicacaoProvider.jsx
@@ -1,17 +1,22 @@
 import { useEffect, useReducer } from "react";
+import PublicacaoContext from "./PublicacaoContext";
+
+import { publicacaoReducer } from "./publicacaoReducer";
+
 import { initialState } from "./inicialState";
-import { publicaoReducer } from "./publicacaoReducer";
 import { useAuth } from "../../hooks/useAuth";
-
 import { localStorageService } from "../../services/localStorageService";
-
 import { PublicacoesService } from "./publicacaoService";
 
 export const PublicacoesProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(publicaoReducer, initialState);
+  const [state, dispatch] = useReducer(publicacaoReducer, initialState);
   const { usuario } = useAuth();
 
   useEffect(() => {
+    if (!usuario?.id) {
+      // Se não houver usuário, você pode lidar com a situação aqui
+      return;
+    }
     const carregarPublicacoes = async () => {
       const armazenadas = localStorageService.ler("publicacoes");
       if (armazenadas) {
@@ -40,11 +45,44 @@ export const PublicacoesProvider = ({ children }) => {
     dispatch({ type: "ADICIONAR_PUBLICACAO", payload: publicacaoCriada });
   };
 
+  const atualizarCampo = (campo, valor) => {
+    dispatch({ type: "ATUALIZAR_CAMPO", payload: { campo, valor } });
+  };
+
+  const removerImagem = () => {
+    dispatch({ type: "REMOVER_IMAGEM" });
+  };
+
+  const adicionarTag = (novaTag) => {
+    dispatch({ type: "ADICIONAR_TAG", payload: novaTag });
+  };
+
+  const removerTag = (tag) => {
+    dispatch({ type: "REMOVER_TAG", payload: tag });
+  };
+
+  const limparTags = () => {
+    dispatch({ type: "LIMPAR_TAGS" });
+  };
+
+  const limparPublicacao = () => {
+    dispatch({ type: "LIMPAR_PUBLICACAO" });
+  };
+
   return (
-    <PublicacoesContext.Provider
-      value={{ publicacoes: state.publicacoes, adicionarPublicacao }}
+    <PublicacaoContext.Provider
+      value={{
+        ...state,
+        adicionarPublicacao,
+        atualizarCampo,
+        removerImagem,
+        adicionarTag,
+        removerTag,
+        limparTags,
+        limparPublicacao,
+      }}
     >
       {children}
-    </PublicacoesContext.Provider>
+    </PublicacaoContext.Provider>
   );
 };
