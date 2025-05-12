656f3132aa0835843ea2c0feb33cee48db41c1c4
Daniel Costa <daniel.dc697@gmail.com>
2025-05-12 10:52:01 -0300

    Ah, isso faz sentido! O AuthProvider precisa estar no nível mais alto para garantir que o PublicacoesProvider consiga acessar o contexto de autenticação corretamente. Quando o AuthProvider está mais baixo na árvore de componentes, o PublicacoesProvider tenta acessar um contexto que ainda não foi fornecido, resultando no erro de undefined.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import { AuthProvider } from "./context/auth/AuthProvider";
 import AppContent from "./components/AppContent";
+import { PublicacoesProvider } from "./context/publicacao/PublicacaoProvider";
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <AppContent />
+        <PublicacoesProvider>
+          <AppContent />
+        </PublicacoesProvider>
       </AuthProvider>
     </Router>
   );
